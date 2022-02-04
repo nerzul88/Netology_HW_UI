fdc4213aab6ce8f68fda70cbb0867385f41881aa
nerzul88 <nerzul.88@gmail.com>
2022-02-04 20:37:12 +0300

    HW1 done

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр Касьянов on 04.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        if let subview = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView {
+//            self.view.addSubview(subview)
+//            subview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        }
+
+    }
+    
+
+}
